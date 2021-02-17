3eaf62c8bd2da0c4585258eed959609b8db9292b
Jonas Hess <jonas.hess@mailbox.org>
2021-02-17 05:04:41 +0100

    move round_time in front of FORM_DATA

--- download.py
+++ download.py
@@ -27,6 +27,20 @@ HEADERS = {
     'Sec-GPC': '1',
 }
 
+def round_time(dt=None, round_to=60):
+    """Round a datetime object to any time lapse in seconds
+    dt : datetime.datetime object, default now.
+    round_to : Closest number of seconds to round to, default 1 minute.
+    Author: Thierry Husson 2012 - Use it as you want but don't blame me.
+    """
+    if dt is None:
+        dt = datetime.datetime.now()
+    if isinstance(dt, int):
+        dt = datetime.datetime.fromtimestamp(round(dt / 1000))
+    seconds = (dt.replace(tzinfo=None) - dt.min).seconds
+    rounding = (seconds+round_to/2) // round_to * round_to
+    return dt + datetime.timedelta(0, rounding-seconds, -dt.microsecond)
+
 FORM_DATA = {
     "request_form": [{
         "format": "XML",
@@ -71,22 +85,6 @@ Module-Ids:
 1001225: Realisierte Erzeugung > Wind Offshore
 5000410: Realisierter Stromverbrauch > Gesamt
 5004359: Realisierter Stromverbrauch > Residuallast
-'''
-
-
-def round_time(dt=None, round_to=60):
-    """Round a datetime object to any time lapse in seconds
-    dt : datetime.datetime object, default now.
-    round_to : Closest number of seconds to round to, default 1 minute.
-    Author: Thierry Husson 2012 - Use it as you want but don't blame me.
-    """
-    if dt is None:
-        dt = datetime.datetime.now()
-    if isinstance(dt, int):
-        dt = datetime.datetime.fromtimestamp(round(dt / 1000))
-    seconds = (dt.replace(tzinfo=None) - dt.min).seconds
-    rounding = (seconds+round_to/2) // round_to * round_to
-    return dt + datetime.timedelta(0, rounding-seconds, -dt.microsecond)
 
 
 class CustomCollector:
